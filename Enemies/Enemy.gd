b03a3f2203dc7c57092a74da693e958cf7ee0196
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2020-11-14 10:15:17 -0300

    Enemy moving speed

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-export var detection_range = 200
+export (int) var detection_range = 200
 export (int) var GRAVITY = 2000
+export (int) var MOVE_SPEED = -20
 
 #Moving variable
 var velocity = Vector2.ZERO
@@ -11,5 +12,5 @@ func _ready():
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
-	velocity.x -= 1
-	velocity = move_and_slide(velocity, Vector2.UP)	
+	velocity.x = MOVE_SPEED # Enemy moving velocity
+	velocity = move_and_slide(velocity, Vector2.UP)
