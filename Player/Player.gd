7cf9ddcb571931ed39bb1c099a298a8e78e49cca
Lucas Fernandes Trizzini <39196373+Ltrizzini@users.noreply.github.com>
2020-11-14 01:40:50 -0300

    player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,10 @@ const ACCELERATION = 10
 const MAX_SPEED = 10
 const FRICTION = 10
 
+var speed: = Vector2(300.0, 1000.0)
+var gravity: = 3000.0
+var velocity: = Vector2.ZERO
+
 # Defining the initial player state
 var state = MOVE
 
@@ -33,7 +37,9 @@ func _physics_process(delta):
 
 # Side move mechanic
 func move_state(delta):
-	pass
+		var direction: = Vector2(
+			Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 0.0
+		)
 
 # Jump mechanic
 func jump_state(delta):
@@ -50,3 +56,21 @@ func run_state(delta):
 # DEAD mechanic
 func dead_state(delta):
 	pass
+	
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") -
+		 Input.get_action_strength("move_left"), 
+		-1.0 if Input. is_action_just_pressed("jump") and is_on_floor() else 1.0
+		)
+func calculate_move_velocity(
+	linear_velocity: Vector2,
+	direction: Vector2,
+	speed: Vector2
+) -> Vector2:
+	var new_velocity: = linear_velocity
+	new_velocity.x = speed.x * direction.x
+	new_velocity.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		new_velocity.y = speed.y * direction.y
+	return speed * direction
