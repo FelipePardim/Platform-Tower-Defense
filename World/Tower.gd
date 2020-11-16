78a0d6b712faddd1e19879dd7a6844b3f3307e4b
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2020-11-16 18:45:28 -0300

    Tweak animations

--- World/Tower.gd
+++ World/Tower.gd
@@ -1,26 +1,33 @@
 extends StaticBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 export (PackedScene) var Projectile
+onready var firePosition = $FirePosition
+onready var timer = $Timer
 
 var enemies = []
 
 func shoot():
-	var b = Projectile.instance()
-	owner.add_child(b)
-	b.transform = $FirePosition.global_transform
+	print(timer.wait_time)
+	if timer.wait_time <= 0.1:
+		var b = Projectile.instance()
+		owner.add_child(b)
+		b.transform = $FirePosition.global_transform
+		timer.start(0.1)
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_home"):
 		shoot()
+		
+	if enemies:
+		var target = enemies.pop_front()
+		var angle = get_angle_to(target.global_position)
+		#firePosition.rotation_degrees = ( - angle)
+		shoot()
+	
+func _on_Range_body_entered(body):
+	if body.is_in_group("Enemies"):
+		enemies.append(body.get_parent())
 
-func _on_Range_area_entered(area):
-	if area.is_in_group("Enemies"):
-		enemies.append(area.get_parent())
-
-func _on_Range_area_exited(area):
-	if area.is_in_group("Enemies"):
-		enemies.erase(area.get_parent())
+func _on_Range_body_exited(body):
+	if body.is_in_group("Enemies"):
+		enemies.erase(body.get_parent())
