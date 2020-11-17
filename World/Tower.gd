d63b53b1d3a915e1bed2ecf6ca098f4b36f1b852
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2020-11-17 16:38:36 -0300

    Tower shooting animation

--- World/Tower.gd
+++ World/Tower.gd
@@ -5,28 +5,41 @@ onready var firePosition = $FirePosition
 onready var timer = $Timer
 onready var animationPlayer = $AnimationPlayer
 
+enum {
+	SHOOTING
+	IDLE
+}
+
 var enemies = []
+var state = IDLE
 
 func shoot():
-	animationPlayer.play("Fire")
 	var b = Bullet.instance()
+	#var target = enemies[0]
 	self.add_child(b)
 	b.transform = $FirePosition.transform
+	#b.position.move_toward(enemies[0].position, 8)
+	#b.position = lerp($FirePosition.position, target.position, 10)
 
 func _physics_process(delta):
+	firePosition.look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("ui_home"):
+		state = SHOOTING
 		shoot()
 		
-	if enemies:
-		var target = enemies.pop_front()
-		var angle = get_angle_to(target.global_position)
-		#firePosition.rotation_degrees = ( - angle)
-		shoot()
+	if state == SHOOTING:
+		animationPlayer.play("Fire")
+	elif state == IDLE:
+		animationPlayer.play("Idle")
 	
 func _on_Range_body_entered(body):
 	if body.is_in_group("Enemies"):
 		enemies.append(body.get_parent())
+		shoot()
 
 func _on_Range_body_exited(body):
 	if body.is_in_group("Enemies"):
 		enemies.erase(body.get_parent())
+		
+func fire_animation_finished():
+	state = IDLE
