90e99dafce813984a68a923d78829e13a25dc26c
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2020-11-17 15:27:22 -0300

    Tweaking the pause system

--- Menu/Pause.gd
+++ Menu/Pause.gd
@@ -5,3 +5,13 @@ func _input(event):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
+
+
+func _on_Resume_pressed():
+	get_tree().paused = false
+	visible = false
+
+
+func _on_Restart_pressed():
+	get_tree().paused = false
+	visible = false
