115c0e117b3c595c8ea8d7f14820002778a1b546
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2020-11-20 22:52:29 -0300

    Implementation of target prioritization for tower's aiming system

--- World/Tower.gd
+++ World/Tower.gd
@@ -4,6 +4,7 @@ export (PackedScene) var Bullet
 onready var firePosition = $FirePosition
 onready var timer = $Timer
 onready var animationPlayer = $AnimationPlayer
+var MAX_RANGE = 280 # Max tower range
 
 enum {
 	SHOOTING
@@ -14,20 +15,29 @@ var enemies = []
 var state = IDLE
 
 func shoot():
-	var b = Bullet.instance()
-	self.add_child(b)
-	
-	# Target enemy
-	var target = enemies[enemies.size() - 1]
-	b.position = $FirePosition.position
-	b.rotation = b.get_angle_to(target.position)
+	if enemies.size() != 0:
+		var b = Bullet.instance()
+		b.position = firePosition.position
+		var targets = enemies
+		var closest_target
+		var closest_target_distance = 999
+		# Prioritazing target by distance
+		for i in range(0, targets.size()):
+			if targets[i] != null:
+				var target_distance = b.get_global_position().distance_to(targets[i].get_global_position())
+				if target_distance < closest_target_distance and target_distance < MAX_RANGE:
+					closest_target = targets[i]
+					closest_target_distance = target_distance
+		# Shooting mechanic and animation
+		if closest_target != null and enemies.size() >= 2:
+			self.add_child(b)
+			state = SHOOTING
+			# Getting the angle to the closest target and shooting
+			b.rotation = b.get_angle_to(closest_target.position)
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Aiming with mouse
 	#firePosition.look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("ui_home"):
-		state = SHOOTING
-		shoot()
 		
 	if state == SHOOTING:
 		animationPlayer.play("Fire")
@@ -37,8 +47,6 @@ func _physics_process(delta):
 func _on_Range_body_entered(body):
 	if body.is_in_group("Enemies"):
 		enemies.append(body)
-		state = SHOOTING
-		shoot()
 
 func _on_Range_body_exited(body):
 	if body.is_in_group("Enemies"):
@@ -46,3 +54,8 @@ func _on_Range_body_exited(body):
 		
 func fire_animation_finished():
 	state = IDLE
+
+
+func _on_Timer_timeout():
+	shoot()
+	timer.start(1)
