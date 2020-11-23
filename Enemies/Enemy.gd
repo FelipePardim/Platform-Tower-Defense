85fdc4870e68c2b46defa11f80deb94a9a51b490
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2020-11-23 16:48:24 -0300

    Enemy stats systeam and hurtbox

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -6,6 +6,8 @@ export (int) var MOVE_SPEED = -20
 
 #Moving variable
 var velocity = Vector2.ZERO
+#Enemy stats
+onready var enemyStats = $Stats
 
 func _ready():
 	pass # Replace with function body.
@@ -15,3 +17,13 @@ func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	velocity.x = MOVE_SPEED # Enemy moving velocity
 	velocity = move_and_slide(velocity, Vector2.UP)
+			
+			
+func _on_HurtBox_area_entered(area):
+	if area.is_in_group("Projectiles"):
+		if enemyStats.health > 0:
+			enemyStats.health -= area.damage
+			print(area)
+		
+		if enemyStats.health <= 0:
+			queue_free()
