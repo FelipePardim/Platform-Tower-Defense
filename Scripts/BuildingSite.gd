010bd8260dd582e69fd78044e0c716189c1c0dba
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2020-12-06 19:58:55 -0300

    Parallax and camera improving

--- Scripts/BuildingSite.gd
+++ Scripts/BuildingSite.gd
@@ -1,13 +1,13 @@
 extends StaticBody2D
 
-export (PackedScene) var Tower
+export (PackedScene) var bowTower
 
 func _ready():
 	pass # Replace with function body.
 
 func _on_Area2D_area_entered(area):
 	if area.is_in_group("PlayerConstructionHitBox"):
-		var tower = Tower.instance()
+		var tower = bowTower.instance()
 		owner.add_child(tower)
 		tower.global_position = $Position2D.global_position
 		queue_free()
